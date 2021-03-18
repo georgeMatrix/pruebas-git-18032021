6cb4fa1854dcf518bb6ee3fdef8909f62fe75061
GeorgeX <jorge@gmail.com>
2021-03-18 12:04:35 -0600

    ClienteRepository completo

--- src/main/java/com/formatoweb/pruebasgit18032021/repository/ClienteRepository.java
+++ src/main/java/com/formatoweb/pruebasgit18032021/repository/ClienteRepository.java
@@ -1,4 +1,7 @@
 package com.formatoweb.pruebasgit18032021.repository;
 
-public interface ClienteRepository {
+import com.formatoweb.pruebasgit18032021.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
