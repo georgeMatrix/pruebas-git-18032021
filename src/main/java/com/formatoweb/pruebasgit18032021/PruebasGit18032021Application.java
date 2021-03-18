4b71c50d0498b6c3fdd4a9c12d0b933d543e9195
GeorgeX <jorge@gmail.com>
2021-03-18 11:46:07 -0600

    primer commit

---
+++ src/main/java/com/formatoweb/pruebasgit18032021/PruebasGit18032021Application.java
@@ -0,0 +1,13 @@
+package com.formatoweb.pruebasgit18032021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasGit18032021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebasGit18032021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/formatoweb/pruebasgit18032021/PruebasGit18032021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formatoweb.pruebasgit18032021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasGit18032021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
