fb284d75d033f04689c47ebe64fbab4198cb0ff5
FormatoWeb <formatoweb@formatoweb.net>
2021-03-18 12:11:03 -0600

    creacion de serviceCliente

---
+++ src/main/java/com/formatoweb/pruebasgit18032021/service/ClienteService.java
@@ -0,0 +1,9 @@
+package com.formatoweb.pruebasgit18032021.service;
+
+import com.formatoweb.pruebasgit18032021.entity.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+    List<Cliente> getClientes();
+}
